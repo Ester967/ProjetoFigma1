73c77a0add6a79fe9943ba6d04634ea4f91b0695
Ester967 <tete19062003@gmail.com>
2022-05-23 15:11:40 -0300

    Create style.js

---
+++ src/pages/Camera/style.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { exp } from "react-native/Libraries/Animated/Easing";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+`;
+
+export const Scroller = styled.ScrollView` 
+    flex: 1;
+`;
