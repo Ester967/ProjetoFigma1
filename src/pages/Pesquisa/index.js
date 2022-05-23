b80475ecde503e595776d33dea12fbbaa58dd274
Ester967 <tete19062003@gmail.com>
2022-05-23 15:13:01 -0300

    Create index.js

---
+++ src/pages/Pesquisa/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container, Scroller} from './style';
+
+
+
+export default function Pesquisa(){
+    return(
+        <Scroller>
+    <Container>
+    </Container>
+    </Scroller>
+    
+    );
+}
