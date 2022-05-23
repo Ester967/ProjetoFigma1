b474811d25dcd16e1f24464d8c1c7e4f1cc6a562
Ester967 <tete19062003@gmail.com>
2022-05-23 15:11:36 -0300

    Create index.js

---
+++ src/pages/Camera/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Scroller, Container} from './style'
+
+
+export default function Camera(){
+
+    return(
+    <Scroller>
+        <Container>
+        </Container>
+    </Scroller>
+     
+
+    
+    );
+}
