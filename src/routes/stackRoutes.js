560336e098f4b42ca4c857af75494bb55f48301c
Ester967 <tete19062003@gmail.com>
2022-05-28 15:57:52 -0300

    Update stackRoutes.js

--- src/routes/stackRoutes.js
+++ src/routes/stackRoutes.js
@@ -10,18 +10,100 @@ import AddSchedule from '../pages/AddSchedule';
 import WorkoutTracker from '../pages/WorkoutTracker'
 import FullbodyWorkout from '../pages/FullbodyWorkout'
 import JumpingJack from '../pages/JumpingJack'
+import StartdWhite from '../pages/StartdWhite';
+import StartdBlue from '../pages/StartdBlue';
+import TrackObjective from '../pages/TrackObjective';
+import GetBurn from '../pages/GetBurn';
+import EatWell from '../pages/EatWell';
+import ImproveQuality from '../pages/ImproveQuality';
+import Register from '../pages/Register';
+import Profile from '../pages/Profile';
+import Login from '../pages/Login';
+import WhatGoal from '../pages/WhatGoal';
+import WhatGoalDois from '../pages/WhatGoalDois';
+import WhatGoalTres from '../pages/WhatGoalTres';
+import Welcome from '../pages/Welcome';
+import MealPlannerScreen from '../pages/MealPlanner';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function StackRoutes(){
     return(
+
         <Stack.Navigator
             screenOptions = {{
             headerShown: false,
-            tabBarShowLabel: false
+            tabBarShowLabel: false,
           }}
         >
-            <Stack.Screen
+            
+        <Stack.Screen 
+        name="StartdWhite" 
+        component={StartdWhite}
+        />
+        
+        <Stack.Screen 
+        name="StartdBlue" 
+        component={StartdBlue}
+        />
+        
+        <Stack.Screen 
+        name="TrackObjective" 
+        component={TrackObjective}
+        />
+        
+        <Stack.Screen 
+        name="GetBurn" 
+        component={GetBurn}
+        />
+        
+        <Stack.Screen 
+        name="EatWell" 
+        component={EatWell}
+        />
+        
+        <Stack.Screen 
+        name="ImproveQuality" 
+        component={ImproveQuality}
+        />
+        
+        <Stack.Screen 
+        name="Register" 
+        component={Register}
+        />
+        
+        <Stack.Screen 
+        name="Profile" 
+        component={Profile}
+        />
+        
+        <Stack.Screen 
+        name="Login" 
+        component={Login}
+        />
+        
+        <Stack.Screen 
+        name="WhatGoal" 
+        component={WhatGoal}
+        />
+        
+        <Stack.Screen 
+        name='WhatGoalDois' 
+        component={WhatGoalDois}
+        />
+        
+        <Stack.Screen 
+        name="WhatGoalTres" 
+        component={WhatGoalTres}
+        />
+        
+        <Stack.Screen 
+        name="Welcome" 
+        component={Welcome}
+        />
+     
+        <Stack.Screen
                 name='Home'
                 component={Routes}
             />
@@ -70,6 +152,13 @@ export default function StackRoutes(){
                 name='JumpingJack'
                 component={JumpingJack}
             />
+
+          <Stack.Screen name="Meal Planner"
+          component={MealPlannerScreen}
+          options={{
+          title: 'Meal Planner',
+          headerTitleAlign: 'center', 
+        }} />
         </Stack.Navigator>
     )
 }
