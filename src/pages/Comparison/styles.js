b369eb8e789048d3d747ea7b487a803902fbdcd2
Ester967 <tete19062003@gmail.com>
2022-05-29 12:48:14 -0300

    Create styles.js

---
+++ src/pages/Comparison/styles.js
@@ -0,0 +1,141 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    background: #FFFFFF;
+`;
+
+export const HeaderButtonLeft = styled.View`
+    top: 18px;
+    margin-left: 10px;
+    left: 10px;
+    position: absolute;
+    border-radius: 10px;
+    border: none;
+`;
+
+export const HeaderButtonRight = styled.View`
+    margin-right: 10px;
+    right: 10px;
+    position: absolute;
+    border-radius: 10px;
+    top: 18px;
+    border: none;
+`;
+
+export const Title = styled.Text`
+    position: absolute;
+    font-style: normal;
+    font-size: 16px;
+    color: #1D1617;
+    text-align: center;
+    top: 16px;
+    left: 140px;
+`;
+
+export const Container1 = styled.View`
+position: absolute;
+width: 315px;
+height: 48px;
+left: 30px;
+top: 102px;
+background: #F7F8F8;
+border-radius: 16px;
+`;
+
+export const Text1 = styled.Text`
+    position: absolute;
+    width: 85px;
+    height: 18px;
+    left: 73px;
+    top: 117px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: #7B6F72;
+`;
+
+export const Text2 = styled.Text`
+    position: absolute;
+    width: 22px;
+    height: 15px;
+    left: 285px;
+    top: 119px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 15px;
+    text-align: right;
+    color: #ADA4A5;
+`;
+
+export const Container2 = styled.View`
+    position: absolute;
+    width: 315px;
+    height: 48px;
+    left: 30px;
+    top: 160px;
+    background: #F7F8F8;
+    border-radius: 16px;
+`;
+
+export const Text3 = styled.Text`
+    position: absolute;
+    width: 88px;
+    height: 18px;
+    left: 73px;
+    top: 175px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: #7B6F72;
+`;
+
+export const Text4 = styled.Text`
+    position: absolute;
+    width: 65px;
+    height: 15px;
+    left: 242px;
+    top: 177px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 15px;
+    text-align: right;
+    color: #ADA4A5;
+`;
+
+export const Button1 = styled.View`
+    justify-content: space-between;
+    border-radius:99px;
+    position: absolute;
+    top:585px;
+    width: 315px;
+    height: 60px;
+    left:31px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 18px 129px;
+    box-shadow: 0px 10px 22px rgba(149, 173, 254, 0.3);
+    border: none;
+`;
+
+export const Title1 = styled.Text`
+    position: absolute;
+    width: 34px;
+    height: 24px;
+    left: 118.5px;
+    top: 20px;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 24px;
+    color: #FFFFFF;
+    flex: none;
+    flex-grow: 0;
+    margin: 0px 10px;
+    justify-content: center;
+`;
