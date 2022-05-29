5a08b4e936e921a3c3eaadc11591e27eb933db32
Ester967 <tete19062003@gmail.com>
2022-05-29 12:50:45 -0300

    Create styles.js

---
+++ src/pages/SleepTracker/styles.js
@@ -0,0 +1,226 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    background: #FFFFFF;
+`;
+
+export const HeaderButtonLeft = styled.View`
+    top: 18px;
+    margin-left: 10px;
+    left: 10px;
+    position: absolute;
+    border-radius: 10px;
+    border: none;
+`;
+
+export const HeaderButtonRight = styled.View`
+    margin-right: 10px;
+    right: 10px;
+    position: absolute;
+    border-radius: 10px;
+    top: 18px;
+    border: none;
+`;
+
+export const Title = styled.Text`
+    position: absolute;
+    font-style: normal;
+    font-size: 16px;
+    color: #1D1617;
+    text-align: center;
+    top: 16px;
+    left: 140px;
+`;
+
+export const Container1 = styled.View`
+    position: absolute;
+    height: 57px;
+    width: 315px;
+    left: 30px;
+    top: 482px;
+    border-radius: 16px;
+    right: 30px;
+    bottom: 310px;
+    opacity: 0.2;
+`;
+
+export const Text1 = styled.Text`
+    position: absolute;
+    left: 50px;
+    right: 178px;
+    top: 500px;
+    bottom: 328px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 21px;
+    color: #1D1617;
+`;
+
+export const Container2 = styled.View`
+    position: absolute;
+    width: 68px;
+    height: 28px;
+    left: 257px;
+    right: 50px;
+    top: 497px;
+    bottom: 324px;
+    border-radius: 50px; 
+`;
+
+export const Text2 = styled.Text`
+    position: absolute;
+    left: 26%;
+    right: 65px;
+    top: 15%;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: #FFFFFF;
+`;
+
+export const Text3 = styled.Text`
+    position: absolute;
+    width: 131px;
+    height: 24px;
+    left: 30px;
+    top: 569px;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    color: #1D1617;
+`;
+
+export const Container3 = styled.View`
+    position: absolute;
+    width: 315px;
+    height: 93px;
+    left: 30px;
+    top: 608px;
+    background: #FFFFFF;
+    box-shadow: 0px 10px 40px rgba(29, 22, 23, 0.07);
+    border-radius: 20px;
+`;
+
+export const Text4 = styled.Text`
+    position: absolute;
+    width: 120px;
+    height: 24px;
+    left: 90px;
+    top: 628px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+    color: #1D1617;
+`;
+
+export const Text5 = styled.Text`
+    position: absolute;
+    width: 145px;
+    height: 24px;
+    left: 90px;
+    top: 657px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    color: #7B6F72;
+`;
+
+export const Container4 = styled.View`
+    position: absolute;
+    width: 315px;
+    height: 93px;
+    left: 30px;
+    top: 716px;
+    background: #FFFFFF;
+    box-shadow: 0px 10px 40px rgba(29, 22, 23, 0.07);
+    border-radius: 20px;
+`;
+
+export const Text6 = styled.Text`
+    position: absolute;
+    width: 100px;
+    height: 24px;
+    left: 90px;
+    top: 736px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+    color: #1D1617;
+`;
+
+export const Text7 = styled.Text`
+    position: absolute;
+    width: 152px;
+    height: 24px;
+    left: 90px;
+    top: 765px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    color: #7B6F72;
+`;
+
+export const Container5 = styled.View`
+    position: absolute;
+    width: 14px;
+    height: 14px;
+    left: 311px;
+    top: 662px;
+    background: #FFFFFF;
+    border-radius: 10px;
+`;
+
+export const Container6 = styled.View`
+    position: absolute;
+    width: 14px;
+    height: 14px;
+    left: 311px;
+    top: 770px;
+    background: #FFFFFF;
+    height: 14px;
+    border-radius: 10px;
+`;
+
+export const Container7 = styled.View`
+    position: absolute;
+    height: 148px;
+    width: 315px;
+    left: 30px;
+    top: 304px;
+    border-radius: 22px;
+    box-shadow: 0px 10px 22px rgba(149, 173, 254, 0.3);
+`;
+
+export const Text8 = styled.Text`
+    position: absolute;
+    height: 21px;
+    width: 113px;
+    left: 50px;
+    top: 324px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 21px;
+    color: #FFFFFF;
+`;
+
+export const Text9 = styled.Text`
+    position: absolute;
+    height: 21px;
+    width: 57px;
+    left: 50px;
+    top: 350px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 21px;
+    color: #FFFFFF;
+`;
