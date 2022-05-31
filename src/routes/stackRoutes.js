25a475022b4376e8c10bdd3007cd0552ce44001b
Ester967 <tete19062003@gmail.com>
2022-05-31 19:46:42 -0300

    Update stackRoutes.js

--- src/routes/stackRoutes.js
+++ src/routes/stackRoutes.js
@@ -23,17 +23,6 @@ import WhatGoal from '../pages/WhatGoal';
 import WhatGoalDois from '../pages/WhatGoalDois';
 import WhatGoalTres from '../pages/WhatGoalTres';
 import Welcome from '../pages/Welcome';
-import MealPlannerScreen from '../pages/MealPlanner';
-import BreakfastScreen from '../pages/Breakfast';
-import BluebarryPancakeScreen from '../pages/BluebarryPancake';
-import MealScheduleScreen from '../pages/MealSchedule';
-import SleepTrackerScreen from '../pages/SleepTracker';
-import SleepScheduleScreen from '../pages/SleepSchedule';
-import AddAlarmScreen from '../pages/AddAlarm';
-import ComparisonScreen from '../pages/Comparison';
-import ResultScreen from '../pages/Result';
-import Result1Screen from '../pages/Result1';
-import Tela6Screen from '../pages/Tela6';
 
 
 const Stack = createNativeStackNavigator();
@@ -162,72 +151,6 @@ export default function StackRoutes(){
                 name='JumpingJack'
                 component={JumpingJack}
             />
-
-          <Stack.Screen name="Meal Planner"
-          component={MealPlannerScreen}
-          options={{
-          title: 'Meal Planner',
-          headerTitleAlign: 'center', 
-        }} />
-
-        <Stack.Screen name="Breakfast" 
-          component={BreakfastScreen}
-          options={{
-          title: 'Breakfast',
-          headerTitleAlign: 'center'
-        }} /> 
-        <Stack.Screen name="Bluebarry Pancake"
-          component={BluebarryPancakeScreen}
-          options={{
-          title: 'Bluebarry Pancake',
-          headerTitleAlign: 'center'
-        }} />
-
-        <Stack.Screen name="Meal Schedule" 
-          component={MealScheduleScreen}
-          options={{
-          title: 'Meal Schedule',
-          headerTitleAlign: 'center',
-        }} />
-        <Stack.Screen name="Sleep Tracker" 
-          component={SleepTrackerScreen}
-          options={{
-          title: 'Sleep Tracker',
-          headerTitleAlign: 'center',
-        }} />
-        <Stack.Screen name="Sleep Schedule" 
-          component={SleepScheduleScreen}
-          options={{
-          title: 'Sleep Schedule',
-          headerTitleAlign: 'center',
-        }} />
-        <Stack.Screen name="Add Alarm" 
-          component={AddAlarmScreen}
-          options={{
-          title: 'Add Alarm',
-          headerTitleAlign: 'center',
-        }} />
-        <Stack.Screen name="Comparison" 
-          component={ComparisonScreen}
-          options={{
-          title: 'Comparison',
-          headerTitleAlign: 'center',
-        }} />
-        <Stack.Screen name="Result" 
-          component={ResultScreen}
-          options={{
-          title: 'Result',
-          headerTitleAlign: 'center',
-        }} />
-        <Stack.Screen name="Result1" 
-          component={Result1Screen}
-          options={{
-          title: 'Result',
-          headerTitleAlign: 'center',
-        }} />
-        <Stack.Screen name="Tela 6" 
-          component={Tela6Screen}
-        />
         </Stack.Navigator>
         
     )
